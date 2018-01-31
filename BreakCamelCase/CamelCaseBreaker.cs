d389e2f403cb7559f2427edc699783bd896a9960
Mike Brooks <PDXmjb@users.noreply.github.com>
2018-01-30 18:48:59 -0800

    Application that takes in a string and adds a space between camel-cased words.
    Changes "helloThisIsCCamelCase" to "hello This Is C Camel Case"

---
+++ BreakCamelCase/CamelCaseBreaker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class CamelCaseBreaker
+{
+    public static string BreakCamelCase(string str)
+    {
+        for (var i = 1; i < str.Length; i++)
+        {
+            // If the first character is lowercase OR uppercase, AND the second char is uppercase...
+            if ((Char.IsLower(str[i - 1]) || Char.IsUpper(str[i - 1])) && Char.IsUpper(str[i]))
+            {
+                // Insert a space before the uppercase character.
+                str = str.Insert(i, " ");
+            }
+        }
+        return str;
+    }
+}
